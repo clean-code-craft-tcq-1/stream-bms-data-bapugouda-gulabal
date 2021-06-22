34168bb6a9e1e6ee2b662eb6afc4346a0a6912e7
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-23 02:53:55 +0530

    Update BMS_Receiver.h

--- BMS_Receiver/BMS_Receiver.h
+++ BMS_Receiver/BMS_Receiver.h
@@ -1,9 +1,29 @@
+/******************************************************************************/
+/*!
+***     \file        BMS_Receiver.h
+***     \author      Abinesh KanjiKovil Thatchinamoorthy 
+***
+***
+***     \brief       Local header for BMS Receiver
+***
+*\n*/
+/*****************************************************************************/
+
+/* Inclusions */
 #include "stdio.h"
 #include "math.h"
 #include "stdlib.h"
 #include "string.h"
 
+/* Constants and Macros */
 
+#define BMS_PARAM_READ_INDEX 12
 
-#define BMS_TEMP_INDEX 12
-#define BMS_SOC_INDEX 15
+const char bmsParam_a[2][20] = {"Temperature", "StateOfChar"};
+
+typedef struct {
+    float bmsParamVal_f[15][2];
+	  int valCount_i;
+}bmsReceiver_t;
+
+/* EOF */
