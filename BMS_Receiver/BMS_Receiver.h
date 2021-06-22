c1da8ff6ce67717baea310d4b2a03e059e62646b
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-23 03:05:55 +0530

    Update BMS_Receiver.h

--- BMS_Receiver/BMS_Receiver.h
+++ BMS_Receiver/BMS_Receiver.h
@@ -22,8 +22,16 @@
 const char bmsParam_a[2][20] = {"Temperature", "StateOfChar"};
 
 typedef struct {
+    float minVal_f;
+    float maxVal_f;
+    float avg_f;
+}paramStat_t;
+
+typedef struct {
     float bmsParamVal_f[15][2];
-	  int valCount_i;
+    paramStat_t tempStat_e;
+    paramStat_t socStat_e;
+    int valCount_i;
 }bmsReceiver_t;
 
 /* Function declarations */
