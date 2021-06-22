438674f4f76a3df11833140656253c27700255d5
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-22 23:26:05 +0530

    Update SenderDataConstants.java

--- src/main/java/sender/SenderDataConstants.java
+++ src/main/java/sender/SenderDataConstants.java
@@ -2,7 +2,7 @@ package sender;
 
 public class SenderDataConstants {
 	public static final String TEMPERTAURE = "Temperature";
-	public static final String STATE_OF_CHARGE = "State Of Charge";
+	public static final String STATE_OF_CHARGE = "StateOfCharge";
 	public static final int LOWER_TEMPERATURE = 0;
 	public static final int HIGHER_TEMPERATURE = 45;
 	public static final int LOWER_SOC = 20;
