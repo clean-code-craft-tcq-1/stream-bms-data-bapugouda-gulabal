c8a429a84d10c6d22bf70ee5931b16ea1f26e795
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-23 01:41:55 +0530

    Update BMS_Receiver.c

--- BMS_Receiver/BMS_Receiver.c
+++ BMS_Receiver/BMS_Receiver.c
@@ -37,7 +37,7 @@ void readBMSdata()
 		printf("Check SSSSvdfgfS : %s \n", soc_buffer);
 		soc_buffer[0]  = strIndexer_c[12+15+cnt_i];
 		printf("Check SSSSvdfgfS : %s \n", soc_buffer);
-		soc_buffer[1]  = strIndexer_c[12+16+cnt_i];
+		soc_buffer[1]  = strIndexer_c[12+15+cnt_i];
 		printf("Check SSSSvdfgfS : %s \n", soc_buffer);
 		
 		printf("Check SSSSS : %c \n", soc_buffer[0]);
