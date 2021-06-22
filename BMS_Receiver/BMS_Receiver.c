846ddd27f6be98152915a02c3ca7207eea7a9cb4
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-23 00:23:04 +0530

    Update BMS_Receiver.c

--- BMS_Receiver/BMS_Receiver.c
+++ BMS_Receiver/BMS_Receiver.c
@@ -24,7 +24,8 @@ void receiveBMSdata()
 	if(NULL != strIndexer_c)
 	{
 	printf("dddddddddd : %s \n", bmsData_a);
-    	while(strIndexer_c[cnt_i] != ',')
+
+    	while(strIndexer_c[cnt_i] != '\,')
     	{
       		temp_buffer[cnt_i] = strIndexer_c[12+cnt_i];
 		printf("Check ewrewdf : %c \n", temp_buffer[cnt_i]);
