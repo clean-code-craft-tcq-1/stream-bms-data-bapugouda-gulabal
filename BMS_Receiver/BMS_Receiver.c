09c8181cf3ef03a5e985f84a3f53775364c83ca2
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-23 01:46:15 +0530

    Update BMS_Receiver.c

--- BMS_Receiver/BMS_Receiver.c
+++ BMS_Receiver/BMS_Receiver.c
@@ -1,26 +1,18 @@
-#include "stdio.h"
-#include "math.h"
-#include "stdlib.h"
-#include "string.h"
+
 
 void readBMSdata(void);
+void analyzeBMSData(char *strIndexer_c);
 
 void readBMSdata() 
 {
   char bmsData_a[500] = {0};
-  char temp_buffer[5] = {0};
-  char soc_buffer[2]  = {0};
   char *strIndexer_c = NULL;
-  float temp_f;
-	int soc_f;
-  int cnt_i=0;
+  
   for(int count = 0;count++ <= 200; count++)
   {
 	bmsData_a[0] = '\0';
 	strIndexer_c = NULL;
-	cnt_i = 0;
 	temp_buffer[0] = '\0';
-	soc_buffer[0] = '\0';
 		
 	scanf("%s", bmsData_a);		
 	  
@@ -28,28 +20,31 @@ void readBMSdata()
   
 	if(NULL != strIndexer_c)
 	{
-		printf("66666666666 : %s \n", strIndexer_c);
-    	while(strIndexer_c[12+cnt_i] != ',')
-    	{
-      		temp_buffer[cnt_i] = strIndexer_c[12+cnt_i];
-			cnt_i++;
-    	}
-		printf("Check SSSSvdfgfS : %s \n", soc_buffer);
-		soc_buffer[0]  = strIndexer_c[12+15+cnt_i];
-		printf("Check SSSSvdfgfS : %s \n", soc_buffer);
-		soc_buffer[1]  = strIndexer_c[12+15+cnt_i];
-		printf("Check SSSSvdfgfS : %s \n", soc_buffer);
-		
-		printf("Check SSSSS : %c \n", soc_buffer[0]);
-		printf("Check SSSSS : %c \n", soc_buffer[1]);
-		printf("Check SSSSS : %s \n", soc_buffer);
-    
-        temp_f = atof(temp_buffer);
-		soc_f  = atoi(soc_buffer);
-        printf("Check Temp : %f \n", temp_f);
-		printf("Check SOC : %d \n", soc_f);
+		analyzeBMSData(strIndexer_c);
 	}
   }
+}
+
+void analyzeBMSData(char *strIndexer_c)
+{
+	int cnt_i=0;
+	char temp_buffer[5] = {0};
+	char soc_buffer[2]  = {0};
+	float temp_f;
+	int soc_i;
+	
+	while(strIndexer_c[12+cnt_i] != ',')
+	{
+		temp_buffer[cnt_i] = strIndexer_c[12+cnt_i];
+		cnt_i++;
+	}
+	soc_buffer[0]  = strIndexer_c[12+15+cnt_i];
+	soc_buffer[1]  = strIndexer_c[12+16+cnt_i];
+
+	temp_f = atof(temp_buffer);
+	soc_i  = ato1(soc_buffer);
+	printf("Check Temp : %f \n", temp_f);
+	printf("Check SOC : %d \n", soc_i);
 }
 
 int main ()
