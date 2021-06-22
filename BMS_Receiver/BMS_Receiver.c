356498122de5453db933f660da1e7504d9c9bed2
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-23 02:53:53 +0530

    Update BMS_Receiver.c

--- BMS_Receiver/BMS_Receiver.c
+++ BMS_Receiver/BMS_Receiver.c
@@ -3,13 +3,11 @@
 void readBMSdata(void);
 void analyzeBMSData(char *strIndexer_c);
 
-const char bmsParam_a[2][20] = {"Temperature", "StateOfChar"};
-float bmsParamVal[15][2] = {0};
-int valCount_i = 0;
+bmsReceiver_t bmsReceiverData_s = {{0}, 0};
 
 void readBMSdata() 
 {
-  char bmsData_a[500] = {0};
+  char bmsData_a[50] = {0};
   char *strIndexer_c = NULL;
   
   for(int count = 0;count++ <= 200; count++)
@@ -31,13 +29,13 @@ void analyzeBMSData(char *bmsData_a)
 		if(NULL != strIndexer_c)
 		{
 			printf("strIndexer_c %s \n",strIndexer_c);
-			while(strIndexer_c[12+cnt_i] != ',')
+			while(strIndexer_c[BMS_PARAM_READ_INDEX+cnt_i] != ',')
 			{
-				temp_buffer[cnt_i] = strIndexer_c[12+cnt_i];
+				temp_buffer[cnt_i] = strIndexer_c[BMS_PARAM_READ_INDEX+cnt_i];
 				cnt_i++;
 			}
-			bmsParamVal[valCount_i][cntParam] = atof(temp_buffer);
-			printf("%s : %f \n ", bmsParam_a[cntParam], bmsParamVal[valCount_i][cntParam]);
+			bmsParamVal_f[valCount_i][cntParam] = atof(temp_buffer);
+			printf("%s : %f \n ", bmsParam_a[cntParam], bmsReceiverData_s.bmsParamVal_f[bmsReceiverData_s.valCount_i][cntParam]);
 		}
 	}	
 	
