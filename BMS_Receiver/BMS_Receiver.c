1193ddbd88ae7ec18193caad1c7b459aed179e42
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-23 15:57:47 +0530

    Update BMS_Receiver.c

--- BMS_Receiver/BMS_Receiver.c
+++ BMS_Receiver/BMS_Receiver.c
@@ -26,16 +26,16 @@ void readBMSdata(char *bmsData_a)
 	{
 		cnt_i = 0;
 		printf("strIndexer_c %s \n",strIndexer_c);
-		/*while(strIndexer_c[12+cnt_i] != ',')
+		while(strIndexer_c[12+cnt_i] != ',')
 		{
 			chTempBuf_a[cnt_i]= strIndexer_c[12+cnt_i];
 			chSOCBuff_a[cnt_i]= strIndexer_c[25+cnt_i];
 			cnt_i++;
-		}*/
-		/*bmsReceiverData_s.bmsParamVal_f[bmsReceiverData_s.valCount_i][0] = atof(chTempBuf_a);
+		}
+		bmsReceiverData_s.bmsParamVal_f[bmsReceiverData_s.valCount_i][0] = atof(chTempBuf_a);
 		bmsReceiverData_s.bmsParamVal_f[bmsReceiverData_s.valCount_i][1] = atof(chSOCBuff_a);
 		printf("%s : %f \n ", bmsParam_a[0], bmsReceiverData_s.bmsParamVal_f[bmsReceiverData_s.valCount_i][0]);
-		printf("%s : %f \n ", bmsParam_a[1], bmsReceiverData_s.bmsParamVal_f[bmsReceiverData_s.valCount_i][1]);*/
+		printf("%s : %f \n ", bmsParam_a[1], bmsReceiverData_s.bmsParamVal_f[bmsReceiverData_s.valCount_i][1]);
 		bmsReceiverData_s.valCount_i++;
 	}
 }
@@ -47,7 +47,7 @@ int main ()
   for(int count = 0;count++ <= 150; count++)
   {
 	scanf("%s", bmsData_a);	
-	//readBMSdata(bmsData_a);
+	readBMSdata(bmsData_a);
   }
 	
   return 0;	
