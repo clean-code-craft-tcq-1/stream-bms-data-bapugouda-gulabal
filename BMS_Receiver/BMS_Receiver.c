10397d1097fb48c9f2569ea37f352458cbc4ecc4
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-23 13:51:34 +0530

    Update BMS_Receiver.c

--- BMS_Receiver/BMS_Receiver.c
+++ BMS_Receiver/BMS_Receiver.c
@@ -29,13 +29,13 @@ void readBMSdata(char *bmsData_a)
 			cnt_i = 0;
 			//temp_buffer[0] ='\0';
 			printf("strIndexer_c %s \n",strIndexer_c);
-			//temp_buffer[0] ='3';
+			temp_buffer[0] ='3';
 			while(strIndexer_c[12+cnt_i] != ',')
 			{
-				temp_buffer[cnt_i] = strIndexer_c[12+cnt_i];
+				/*temp_buffer[cnt_i] = strIndexer_c[12+cnt_i];
 				printf("------------cnt_i :: %d \n", cnt_i);
 				printf("asdfsdfdsfsfsd %c \n",strIndexer_c[12+cnt_i]);
-				printf("temp_buffer %s \n",temp_buffer);
+				printf("temp_buffer %s \n",temp_buffer);*/
 				cnt_i++;
 			}
 			bmsReceiverData_s.bmsParamVal_f[bmsReceiverData_s.valCount_i][cntParam] = atof(temp_buffer);
@@ -52,7 +52,7 @@ int main ()
   for(int count = 0;count++ <= 200; count++)
   {
 	scanf("%s", bmsData_a);	
-	//readBMSdata(bmsData_a);
+	readBMSdata(bmsData_a);
   }
 	
   return 0;	
